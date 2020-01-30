c7d245754a96780a337b5b4adf2e2e165306dade
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-01-30 11:36:03 -0500

    Able to add images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+    let fetchDogs = fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(dogs => dogs.message.forEach(dogObj => renderDogs(dogObj)))
+
+})
+
+function renderDogs(dogObject) {
+
+    // building the dog image
+   let dogImg = document.createElement("img") 
+   dogImg.src = dogObject
+   imgContainer().appendChild(dogImg)
+    
+}
+
+//get elements
+function imgContainer(){
+    return document.getElementById("dog-image-container")
+}
